907bba78c858cdce1b5e022d2bbd6119a897e49b
yeongjong310 <tohero21@naver.com>
2022-01-20 17:22:25 +0900

    feat: Tree constructor, createTree 구현

--- datastructure/tree/binaryTree.ts
+++ datastructure/tree/binaryTree.ts
@@ -3,3 +3,27 @@ interface BinaryTreeNode<T> {
   left?: BinaryTreeNode<T>;
   right?: BinaryTreeNode<T>;
 }
+
+abstract class Tree<T> {
+  root?: BinaryTreeNode<T> | T;
+  constructor(root?: BinaryTreeNode<T> | T, arr?: T[]) {
+    if (root) {
+      this.root = root;
+    } else if (arr) {
+      this.root = this.createTree(arr);
+    }
+  }
+
+  createTree(arr: T[]): BinaryTreeNode<T> {
+    if (arr.length === 0) {
+      return undefined;
+    }
+    const mid = Math.floor(arr.length / 2);
+    const root = {
+      val: arr[mid],
+      left: this.createTree(arr.slice(0, mid)),
+      right: this.createTree(arr.slice(mid + 1)),
+    };
+    return root;
+  }
+}
