23d73df12cb570e34cf610f240e906b433e4ba4b
yeongjong <tohero21@naver.com>
2022-03-10 17:09:43 +0900

    Config - eslint js 폴더 내부로 이동 및 eslint,prettier충돌제거

---
+++ js/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 'off',
+    'no-else-return': 'off',
+  },
+  extends: ['prettier'],
+  plugins: ['prettier'],
+};
