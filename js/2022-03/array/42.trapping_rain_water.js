793983b5d476981ce6491d0cc452d664d010c14b
yeongjong <tohero21@naver.com>
2022-03-17 20:59:35 +0900

    Add - 투포인터 방식 풀이 추가

--- js/2022-03/array/42.trapping_rain_water.js
+++ js/2022-03/array/42.trapping_rain_water.js
@@ -10,7 +10,7 @@
 // [2, 1, 0, 1, 3] 과 같은 경우
 // [2, 1, 3] 으로 변경되며 2와 3 사이의 채워질 빛물 높이(2 - 1)를 구할 수 있다.
 
-const trap = function (heights) {
+const trap1 = function (heights) {
   const stack = [];
   let volume = 0;
 
@@ -32,6 +32,39 @@ const trap = function (heights) {
 
     stack.push(index);
   });
+
+  return volume;
+};
+
+// two pointer
+// 핵심 원리: 현재 index를 기준으로 지나온 height중 가장 큰 높이까지 빗물을 채우는 방식이다.
+// 이동하는 pointer는 heights중 최대 높이(전체 최대 높이)까지만 이동하기 때문에, 핵심 원리의 최대 높이는 그 보다 작거나 같다.
+// 즉, 지나온 최대 높이와 다음 최대 높이(전체 최대 높이가 마지막) 사이에 지나온 최대 높이 까지 각 위치의 물을 채워나가는 방식
+// [1,0,2,0,3] 의 경우
+// index 1 기준 지나온 최대 높이는 index 0의 1이기 떄문에 1로 채워 넣는다. (빗물은 장벽중 낮은 높이까지만 차며 그 이상은 흘러내리기 때문)
+// [1,1,2,0,3] 이 되며 index 2 기준으로는 최대 높이가 자신 index 2이기 때문에 빗물은 0 이다.
+// [1,1,2,0,3] 에서 index 3 기준으로 최대 높이는 index 2의 2 이기 때문에 빗물 2를 채워 넣는다.
+
+const trap2 = function (heights) {
+  let left = 0;
+  let right = heights.length - 1;
+  let volume = 0;
+
+  let leftMax = heights[left];
+  let rightMax = heights[right];
+
+  while (left !== right) {
+    leftMax = Math.max(heights[left], leftMax);
+    rightMax = Math.max(heights[right], rightMax);
+
+    if (leftMax <= rightMax) {
+      volume += leftMax - heights[left];
+      left++;
+    } else {
+      volume += rightMax - heights[right];
+      right--;
+    }
+  }
 
   return volume;
 };
